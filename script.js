d4dacf7cfb11e0ca0571e9e53c39e4f7b3103f1a
Hong Ng <hong.yang.ng@gmail.com>
2023-05-28 22:23:03 +1000

    Update JS script to take 2 numbers and 1 math operation, add funtion to return the result of a chosen operation

--- script.js
+++ script.js
@@ -28,21 +28,106 @@ const divide = function(num1, num2){
 }
 
 const operate = function(operator, num1, num2){
+    let result = '';
+
     switch(operator){
-        case 'add':
-            console.log(add(num1, num2));
+        case '+':
+            result = add(num1, num2);
             break;
-        case 'substract':
-            console.log(subtract(num1, num2));
+        case '-':
+            result = subtract(num1, num2);
             break;
-        case 'multiply':
-            console.log(multiply(num1, num2));
+        case '×':
+            result = multiply(num1, num2);
             break;
-        case 'divide':
-            console.log(divide(num1, num2));
+        case '÷':
+            result = divide(num1, num2);
             break;
         default:
             console.log("Please choose a valid option.")
             break;
     }
+
+    return result;
 }
+
+//Numbers
+let operation = {
+    num1: '',
+    opType: '',
+    num2: ''
+};
+
+//Select screen
+const screen = document.getElementById("calculator-screen-data");
+
+//Select numeric buttons 
+let numbers = document.querySelectorAll(".calculator-number");
+
+numbers.forEach(number => {
+
+    number.addEventListener("click", function(e){
+        let num = e.target.innerText;
+
+        //If the object has an operation, the number being pressed 
+        //is placed into num2.
+        if(operation.opType != ''){
+            //Clear screen if 
+            if(operation.num2 == ''){
+                screen.innerText = '';
+            }
+
+            screen.innerText += num;
+            operation.num2 = screen.innerText;
+
+            console.log(operation);
+            console.log("True, it has an op type.")
+        }
+
+        //If the object does not have an operation yet, the number 
+        //being pressed is placed into num1.
+        else {
+            //Update screen
+            screen.innerText += num;
+
+            //Update object num1
+            operation.num1 = screen.innerText;
+            console.log(operation);
+
+        }
+
+    })
+})
+
+let operators = document.querySelectorAll(".calculator-operation");
+
+operators.forEach(ops => {
+
+    ops.addEventListener("click", function(e){
+        let op = e.target.innerText;
+
+        //Update object opType
+        operation.opType = op; 
+        console.log(operation);
+    })
+})
+
+let evaluate = document.getElementById("evaluate");
+
+evaluate.addEventListener("click", function(e){
+    let opType = operation.opType;
+    let num1 = parseInt(operation.num1);
+    let num2 = parseInt(operation.num2);
+    let opResult = '';
+    
+    console.log("num1: " + num1);
+    console.log("num2: " + num2);
+    console.log("opType " + opType);
+
+    //Call the master operate function
+    opResult = operate(opType, num1, num2);
+
+    //Update calculator screen
+    screen.innerText = opResult;
+
+})
